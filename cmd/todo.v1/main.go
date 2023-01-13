16c0caa19e970187d9198224b871bc17b7a5f5bc
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-13 15:01:49 +0800

    feat: add logic for 5 switch cases based on the flags
    list, complete and task are pretty self explanatory
    
    nit-pickingly extracted the logic to avoid 1 indentation for the list
    logic, but then ended up reusing the function in the 4th case checking
    if there are no flags provided, that was lucky. decided to keep this
    functionality, just because it seemed like there should be a default
    behavior when the cli is invoked without any arguments
    
    default case is to return an error message if an invalid option is used

--- cmd/todo.v1/main.go
+++ cmd/todo.v1/main.go
@@ -13,7 +13,9 @@ const todoFileName = ".todo.json"
 func main() {
 	// Parsing command line flags
 	task := flag.String("task", "", "Task to be included in the ToDo list")
-	list := flag.Bool("list", false, "List all tasks")
+	// this flag doesn't need to return val since we will use container to
+	// usedFlag map to check for presence which is already a bool
+	flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
 	flag.Parse()
@@ -37,9 +39,51 @@ func main() {
 
 	// Decide what to do based on the flag used
 	switch {
-	case usedFlag["task"]:
-	case usedFlag["completed"]:
 	case usedFlag["list"]:
+		listToDoItems(l)
+	case usedFlag["completed"]:
+		// Complete the given item
+		err := l.Complete(*complete)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		// Save the new list
+		err = l.Save(todoFileName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case usedFlag["task"]:
+		l.Add(*task)
+
+		err := l.Save(todoFileName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	case len(os.Args) == 1:
+		// no arguments provided
+		listToDoItems(l)
 	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
+	}
+}
+
+// extracting this logic to make switch statement more readable
+// by minimizing indents
+func listToDoItems(l *basic_todo_app.List) {
+	// List current to do items if itemsList is not empty
+	if len(*l) <= 0 {
+		fmt.Println("You have no to do items")
+		os.Exit(0)
+	}
+	for _, item := range *l {
+		if !item.Done {
+			fmt.Println(item.Task)
+		}
 	}
 }
