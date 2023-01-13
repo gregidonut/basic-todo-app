56a2eecad6b5773c6e6d3429714bdce93db3d235
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-13 08:27:55 +0800

    test: fill in a generic fixture for TestMain()
    initial test case implementation to facilitate the set up and tear down
    of the the planned test I believe the term for this is fixture

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -1,12 +1,40 @@
 package main_test
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
+	"testing"
+)
 
 var (
 	binName  = "todo"
 	fileName = ".todo.json"
 )
 
+// TestMain executes the go build tool to create the binary of the cli
+// then runs it with arguments to check if outputs are expected
+// then cleans up the files after the function is completed.
 func TestMain(m *testing.M) {
+	fmt.Println("Building tool...")
 
+	if runtime.GOOS == "windows" {
+		binName += ".exe"
+	}
+
+	build := exec.Command("go", "build", "-o", binName)
+	err := build.Run()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot build tool %q: %s", binName, err)
+	}
+
+	fmt.Println("Running tests...")
+	result := m.Run()
+
+	fmt.Println("Cleaning up...")
+	os.Remove(binName)
+	os.Remove(fileName)
+
+	os.Exit(result)
 }
