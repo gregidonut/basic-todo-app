6ad0bab82161a1a6cf19dea38b8d1596e7abcb14
Gregory Babajide V. Osilaja <gregosilaja@outlook.com>
2023-01-13 12:12:39 +0800

    feat: use flag package to add 3 important flags
    instead of using os.Args[1:] which can get very complicated when adding
    flags and to help with scaling
    
    setup some initial flags to use the api

--- cmd/todo.v1/main.go
+++ cmd/todo.v1/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gregidonut/basic_todo_app"
 	"os"
@@ -11,6 +12,13 @@ import (
 const todoFileName = ".todo.json"
 
 func main() {
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+
+	flag.Parse()
+
 	l := &basic_todo_app.List{}
 
 	// Use the Get method to read to do items from file
